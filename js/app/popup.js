e2c3ea2976c23274834e30509d5a2dbedc3a65be
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2013-05-24 09:11:13 +0100

    Added Google Analytics and default tabsets in scope to a new array if there isn't one in storage so that the .push() doesn't fail later.

--- js/app/popup.js
+++ js/app/popup.js
@@ -1,7 +1,17 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-5243256-6']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 myApp.controller('TabsetsController', function($scope) {
 	chrome.storage.sync.get(null, function(blob) {
 		console.log(blob);
-		$scope.tabsets = blob.tabsets;
+		$scope.tabsets = blob.tabsets || [];
 		$scope.$apply();
 	});
 
